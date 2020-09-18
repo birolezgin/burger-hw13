59ddb9313107a05da07ad7417e453f91b99b3307
birolezgin <b.ezginksf@gmail.com>
2020-09-18 14:53:55 -0700

    seeds.sql created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Meat Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper",true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheese", true);
