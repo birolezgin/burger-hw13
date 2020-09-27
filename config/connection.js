f11320814aced6670712b72aa817ec6dc1b05595
birolezgin <b.ezginksf@gmail.com>
2020-09-26 21:53:20 -0700

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,22 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "nqyobgqys14vn98k",
-    password: "e6o1t87ywtogiljr",
-    database: "	ujy7f2aj4b9m98nj" 
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "243866be.",
+    database: "burgers_db" 
 });
 };
 
-connection.connect();
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
+// Export connection for our ORM to use.
 module.exports = connection;
